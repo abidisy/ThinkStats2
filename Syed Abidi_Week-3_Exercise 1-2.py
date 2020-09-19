3a2f6bd101c302dd8d65f8f339092e6b63281072
syed_abidi <53982585+abidisy@users.noreply.github.com>
2020-09-18 22:57:24 -0500

    Add files via upload

---
+++ Syed Abidi_Week-3_Exercise 1-2.py
@@ -0,0 +1,10 @@
+# This program will perform solution for Exercise 1.2 of ThinkStat2 book, page 14.
+# TASK 1: Write a function that reads the respondent file, 2002FemResp.dat.gz.
+# TASK 2: Print the value counts for Pregnum variable and compare them to the published results in the NSFG codebook.
+
+
+from __future__ import print_function, division
+
+import sys
+
+
